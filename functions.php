6f27bfcc7cf437998cb510a03d1956ade03e37bb
ajsmorillas <antonio@imorillas.com>
2024-02-19 12:05:18 +0100

    Update functions.php

--- functions.php
+++ functions.php
@@ -22,17 +22,3 @@ function head_tracking(){
 ADD YOUR CODE HERE
 <?php
 };
-
-/**
- * Disable jQuery Migrate in WordPress.
- *
- * @author Guy Dumais.
- * @link https://en.guydumais.digital/disable-jquery-migrate-in-wordpress/
- */
-add_filter( 'wp_default_scripts', $af = static function( &$scripts) {
-    if(!is_admin()) {
-        $scripts->remove( 'jquery');
-        $scripts->add( 'jquery', false, array( 'jquery-core' ), '1.12.4' );
-    }    
-}, PHP_INT_MAX );
-unset( $af );
